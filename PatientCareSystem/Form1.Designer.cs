1d934d89ce668564ee12f3251fbc4fa30dc27c74
jiiminhong <chunsim015@gmail.com>
2024-05-24 22:21:19 +0900

    feat: basic form and nav

--- PatientCareSystem/Form1.Designer.cs
+++ PatientCareSystem/Form1.Designer.cs
@@ -28,13 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAppointment = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnPatientList = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlNav = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.Controls.Add(this.pnlNav);
+            this.panel1.Controls.Add(this.btnAppointment);
+            this.panel1.Controls.Add(this.btnSettings);
+            this.panel1.Controls.Add(this.btnPatientList);
+            this.panel1.Controls.Add(this.btnDashboard);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(188, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnAppointment
+            // 
+            this.btnAppointment.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAppointment.FlatAppearance.BorderSize = 0;
+            this.btnAppointment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAppointment.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAppointment.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(172)))), ((int)(((byte)(236)))));
+            this.btnAppointment.Image = global::PatientCareSystem.Properties.Resources.free_icon_calendar_661553;
+            this.btnAppointment.Location = new System.Drawing.Point(0, 237);
+            this.btnAppointment.Name = "btnAppointment";
+            this.btnAppointment.Size = new System.Drawing.Size(188, 42);
+            this.btnAppointment.TabIndex = 1;
+            this.btnAppointment.Text = "Appointment";
+            this.btnAppointment.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAppointment.UseVisualStyleBackColor = true;
+            this.btnAppointment.Click += new System.EventHandler(this.btnAppointment_Click);
+            this.btnAppointment.Leave += new System.EventHandler(this.btnAppointment_Leave);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(172)))), ((int)(((byte)(236)))));
+            this.btnSettings.Image = global::PatientCareSystem.Properties.Resources.free_icon_settings_4720747;
+            this.btnSettings.Location = new System.Drawing.Point(0, 535);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(186, 42);
+            this.btnSettings.TabIndex = 1;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            this.btnSettings.Leave += new System.EventHandler(this.btnSettings_Leave);
+            // 
+            // btnPatientList
+            // 
+            this.btnPatientList.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPatientList.FlatAppearance.BorderSize = 0;
+            this.btnPatientList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPatientList.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPatientList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(172)))), ((int)(((byte)(236)))));
+            this.btnPatientList.Image = global::PatientCareSystem.Properties.Resources.free_icon_list_items_7427790;
+            this.btnPatientList.Location = new System.Drawing.Point(0, 195);
+            this.btnPatientList.Name = "btnPatientList";
+            this.btnPatientList.Size = new System.Drawing.Size(188, 42);
+            this.btnPatientList.TabIndex = 1;
+            this.btnPatientList.Text = "Patient List";
+            this.btnPatientList.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnPatientList.UseVisualStyleBackColor = true;
+            this.btnPatientList.Click += new System.EventHandler(this.btnPatientList_Click);
+            this.btnPatientList.Leave += new System.EventHandler(this.btnPatientList_Leave);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(172)))), ((int)(((byte)(236)))));
+            this.btnDashboard.Image = global::PatientCareSystem.Properties.Resources.free_icon_home_747589;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 153);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(188, 42);
+            this.btnDashboard.TabIndex = 1;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+            this.btnDashboard.Leave += new System.EventHandler(this.btnDashboard_Leave);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(188, 153);
+            this.panel2.TabIndex = 0;
+            // 
+            // pnlNav
+            // 
+            this.pnlNav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(172)))), ((int)(((byte)(236)))));
+            this.pnlNav.Location = new System.Drawing.Point(0, 159);
+            this.pnlNav.Name = "pnlNav";
+            this.pnlNav.Size = new System.Drawing.Size(3, 100);
+            this.pnlNav.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(180)))), ((int)(((byte)(194)))));
+            this.label2.Location = new System.Drawing.Point(40, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Writing some Texts";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(172)))), ((int)(((byte)(236)))));
+            this.label1.Location = new System.Drawing.Point(48, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PatientCareSystem.Properties.Resources.free_icon_user_1177568;
+            this.pictureBox1.Location = new System.Drawing.Point(60, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(951, 577);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnAppointment;
+        private System.Windows.Forms.Button btnPatientList;
+        private System.Windows.Forms.Panel pnlNav;
     }
 }
 
--- PatientCareSystem/Form1.cs
+++ PatientCareSystem/Form1.cs
@@ -7,14 +7,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace PatientCareSystem
 {
     public partial class Form1 : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
+
         public Form1()
         {
             InitializeComponent();
+
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+            pnlNav.Height = btnDashboard.Height;
+            pnlNav.Top = btnDashboard.Top;
+            pnlNav.Left = btnDashboard.Left;
+            btnDashboard.BackColor = Color.FromArgb(244, 244, 239);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnDashboard.Height;
+            pnlNav.Top = btnDashboard.Top;
+            pnlNav.Left = btnDashboard.Left;
+            btnDashboard.BackColor = Color.FromArgb(244, 244, 239);
+        }
+
+        private void btnPatientList_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnPatientList.Height;
+            pnlNav.Top = btnPatientList.Top;
+            btnPatientList.BackColor = Color.FromArgb(244, 244, 239);
+        }
+
+        private void btnAppointment_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnAppointment.Height;
+            pnlNav.Top = btnAppointment.Top;
+            btnAppointment.BackColor = Color.FromArgb(244, 244, 239);
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnSettings.Height;
+            pnlNav.Top = btnSettings.Top;
+            btnSettings.BackColor = Color.FromArgb(244, 244, 239);
+        }
+
+        private void btnDashboard_Leave(object sender, EventArgs e)
+        {
+            btnDashboard.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnPatientList_Leave(object sender, EventArgs e)
+        {
+            btnPatientList.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnAppointment_Leave(object sender, EventArgs e)
+        {
+            btnAppointment.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnSettings_Leave(object sender, EventArgs e)
+        {
+            btnSettings.BackColor = Color.FromArgb(24, 30, 54);
         }
     }
 }
